61d30928091230e669bdf1bbc2d86e59bce8654c
parchurimadankumar01 <parchurimadankumar01@gmail.com>
2026-01-24 10:18:35 +0530

    update

--- javascript.js
+++ javascript.js
@@ -6,3 +6,9 @@ document.getElementById("btn").addEventListener("click", Hello)
 function Hello(){
     alert("button function changed")
 }
+
+// if i want use class for  AddEventLister
+
+document.querySelector(".btn").addEventListener('click', function(){
+    alert('clicked using class')
+})
